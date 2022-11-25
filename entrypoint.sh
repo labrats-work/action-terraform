5f2f17f5ddcebe518dadd32ea6d7a7e5384d9afe
tompisula <tompisula@labrats.work>
2022-11-25 10:01:36 +0100

    add INPUT_VERB

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,25 @@ set -o pipefail
 # get version
 terraformVersion=$(terraform version | head -n 1 | cut -d ' ' -f 2)
 # output terraformVersion
-echo "ansibleVersion=$terraformVersion" >> $GITHUB_OUTPUT
+echo "terraformVersion=$terraformVersion" >> $GITHUB_OUTPUT
+
+export VERB="apply"
+if [ ! -z "$INPUT_VERB" ]
+then
+  echo "\$INPUT_VERB is set to ${INPUT_VERB}."
+    case "$INPUT_VERB" in
+        "plan") export VERB="plan" 
+        ;;
+        "apply") export VERB="apply" 
+        ;;
+        "destroy") export VERB="destroy" 
+        ;;
+    esac
+else
+  echo "\$INPUT_VERB not set."
+  exit 1
+fi
+
+echo "going to execute: "
+echo terraform ${VERB} --var-file=default.tfvars
+terraform ${VERB} --var-file=default.tfvars
