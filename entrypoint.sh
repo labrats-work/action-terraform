bdbbec47d7da51f5a0af9d05b04cd5007fb8b079
tompisula <tompisula@labrats.work>
2022-11-25 12:36:54 +0100

    changes for plan

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,16 +26,16 @@ then
 fi
 
 # Evaluate INPUT_VERB
-export VERB="apply"
+export TF_VERB="apply"
 if [ ! -z "$INPUT_VERB" ]
 then
   echo "\$INPUT_VERB is set to ${INPUT_VERB}."
     case "$INPUT_VERB" in
-        "plan") export VERB="plan" 
+        "plan") export TF_VERB="plan" 
         ;;
-        "apply") export VERB="apply" 
+        "apply") export TF_VERB="apply" 
         ;;
-        "destroy") export VERB="destroy" 
+        "destroy") export TF_VERB="destroy" 
         ;;
     esac
 else
@@ -43,19 +43,24 @@ else
   exit 1
 fi
 
-# Evaluate AUTOAPPLY
-export AUTOAPPLY=""
-if [ "$VERB" = "apply" ] || [ "$VERB" = "destroy" ] 
+export TF_AUTOAPPROVE=""
+if [ "$TF_VERB" = "apply" ] || [ "$TF_VERB" = "destroy" ] 
 then
-  export AUTOAPPLY="-auto-approve"
+  export TF_AUTOAPPROVE="-auto-approve"
+fi
+
+export TF_OUT=""
+if [ "$TF_VERB" = "plan" ]
+then
+  export TF_OUT="-out=plan.tf"
 fi
 
 # Evaluate INPUT_VARSFILE
-export VARSFILE=
+export TF_VARSFILE=
 if [ ! -z "$INPUT_VARSFILE" ]
 then
   echo "\$INPUT_VARSFILE is set. Using $INPUT_VARSFILE."
-  export VARSFILE="--var-file=$INPUT_VARSFILE"
+  export TF_VARSFILE="--var-file=$INPUT_VARSFILE"
 fi
 
 # Evaluate INPUT_INIT
@@ -67,10 +72,10 @@ then
 fi
 
 echo "going to execute: "
-echo terraform ${VERB} ${VARSFILE} ${AUTOAPPLY}
-terraform ${VERB} ${VARSFILE} ${AUTOAPPLY} | tee tf.out
+echo terraform ${TF_VERB} ${TF_VARSFILE} ${TF_AUTOAPPROVE} ${TF_OUT}
+terraform ${TF_VERB} ${TF_VARSFILE} ${TF_AUTOAPPROVE} ${TF_OUT}
 
-if [ "$VERB" = "plan" ]
+if [ "$TF_VERB" = "plan" ]
 then
     echo 'TF_PLAN<<EOF' >> $GITHUB_OUTPUT
     cat tf.out >> $GITHUB_OUTPUT
